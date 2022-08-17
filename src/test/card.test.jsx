6462e3a396e44fd879dc61db870a7ad89697b1b5
pee47kay <papakowdadson@gmail.com>
2022-08-17 16:25:20 +0000

    starting with test

---
+++ src/test/card.test.jsx
@@ -0,0 +1,8 @@
+import reactTestRenderer from 'react-test-renderer'
+import Card from '../component/shared/card'
+
+it('creates a card component',()=>{
+    const component = reactTestRenderer.create(
+        <Card></Card>
+    )
+});
---
+++ src/test/spinner.test.jsx
@@ -0,0 +1,9 @@
+import reactTestRenderer from "react-test-renderer";
+import renderer from "react-test-renderer";
+import Spinner from "../component/shared/spinner";
+
+it('creates a loading component',()=>{
+    reactTestRenderer.create(
+       <Spinner></Spinner>
+    )
+});
